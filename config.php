63df1127b1eb4106680dd5c79855f8a900f5bc74
thiago.machado <thiago.machado@69b8bcb9-e54a-2cff-a30e-325e6f696b92>
2011-04-20 21:03:10 +0000

    Separação para variaveis de base de url e path local, de produção ou qualquer outra espécie

--- config.php
+++ config.php
@@ -1,55 +1,63 @@
 <?php
 define("Titulo","Clube de Lideres Online"); //define titulo padrao
-
 define("Ambiente","local"); //prod e local
 
-// PRODUCAO
-/*$dbconfig['prod'] = array(
-"driver"=>"mysql",
-"host"=>"sportweb.fatcowmysql.com",
-"db"=>"atreyo",
-"user"=>"atreyo",
-"pass"=>"s7c8e9"
-);*/
+$_base['prod'] = array
+(
+	"base"		=>	"/home/users/web/b568/moo.sportweb/atreyocom/LABS/clo",
+	"base_url"	=>	"http://www.clubedelider.org/"
+);
 
-$dbconfig['prod'] = array(
-"driver"=>"mysql",
-"host"=>"mysql.ants.com.br",
-"db"=>"ants_dev",
-"user"=>"antscomunicacao",
-"pass"=>"ants2201"
+$_base['local'] = array
+(
+	"base"		=>	"/var/www/dementor/framework-dementor",
+	"base_url"	=>	"http://localhost/dementor/framework-dementor/"
+);
+
+
+$dbconfig['prod'] = array
+(
+	"driver"	=>	"mysql",
+	"host"  	=>	"mysql.ants.com.br",
+	"db"		=>	"ants_dev",
+	"user"		=>	"antscomunicacao",
+	"pass"		=>	"ants2201"
 );
 
 
 // TESTE/LOCAL
-$dbconfig['local'] = array(
-"driver"=>"mysql",
-"host"=>"localhost",
-"db"=>"clo",
-"user"=>"root",
-"pass"=>"root"
+$dbconfig['local'] = array
+(
+	"driver"	=>	"mysql",
+	"host"		=>	"localhost",
+	"db"		=>	"clo",
+	"user"		=>	"root",
+	"pass"		=>	"root"
 );
 
 
 switch(Ambiente){
 	case "prod":
 		//define("base","/home1/atreyoco/public_html/clubedelider");
-		define("base","/home/users/web/b568/moo.sportweb/atreyocom/LABS/clo");
+		//define("base",$_base[Ambiente]["base"]);
 		//define("base","/home/antscomunicacao/ants.com.br/clientes/dublado");
-		define("base_url","http://www.clubedelider.org/"); 
+		//define("base_url",$_base[Ambiente]["base_url"]); 
 		//define("base_url","http://atreyo.com/LABS/clo/"); 
 		//define("base_url","http://www.ants.com.br/clientes/dublado/"); 
 	break;
 	default:
-		define("base","/var/www/clo");
-		define("base_url","http://localhost/clo/"); 
+		//define("base","/var/www/dementor/framework-dementor");
+		//define("base_url","http://localhost/dementor/framework-dementor/"); 
 	break;
 }
+
+define("base","/var/www/dementor/framework-dementor");
+define("base_url","http://localhost/dementor/framework-dementor/"); 
 
 set_include_path(base);
 
 if(!file_exists(base)){
-echo "Defina Base(sugestão): " . $_SERVER["DOCUMENT_ROOT"];
+echo "Defina Base(sugestão): " . $_SERVER["DOCUMENT_ROOT"] . '<br>';
 }
 
 //define("mode","dev"); //mode = dev ativa o depurador _GET _POST _REQUEST
@@ -57,4 +65,4 @@ define("error_log",true);
 // true para exeuctar error_log e false para imprimir na tela
 // algum servidores não deixam acessar o log diretamente do arquivo
 
-define("error-ip","192.168.0.103"); //define ip que verá a depuração
+define("error-ip","192.168.1.113"); //define ip que verá a depuração
